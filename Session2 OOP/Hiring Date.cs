a5e05de4fe8dbcf78b39b2f6bde0610d89f2519a
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2024-12-25 03:02:43 +0200

    Assignment

---
+++ Session2 OOP/Hiring Date.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session2_OOP
+{
+    internal class Hiring_Date
+    {
+        public int date {  get; set; }
+        public int year { get; set; }
+        public int month { get; set; }
+        public Hiring_Date(int _date, int _month, int _year)
+        {
+            date = _date;
+            year = _year;
+            month = _month;
+        }
+    }
+}
