5cf097b79c681dd0108f304e097d51a00b8ecaac
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-01 23:33:08 +0200

    Encapsulation Pt.1

---
+++ Session 2 OOP Demo/Encapsulation/Employee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_2_OOP_Demo.Encapsulation
+{
+    internal struct Employee
+    {
+        // encapsulation is separating data definition from its use (getters & setters)
+        #region Attributes
+        private int id;
+        private string? name;
+        private decimal salary;
+        //private decimal deduction; //derived attribute
+        #endregion
+
+        #region getter & setter
+        //1. getter and setters
+        public int getid() => id;
+        public void setid(int id)
+        {
+            this.id = id > 1000 && id < 10000 ? id : 9999;
+        }
+        #endregion
+
+        #region property
+        public string? Name
+        {
+            get
+            {
+                return name;
+            }
+            set //init
+            {
+                //data validation
+                name = validatename(value);
+            }
+
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = validatesalary(value); }
+        }
+        public decimal Deduction
+        {
+            get { return salary * 0.2M; }
+        }
+        //this approach is recommended
+        #endregion
+
+        #region Constructor
+        public Employee(int id, string? name, decimal salary)
+        {
+            this.id = id;
+            this.name = validatename(name);
+            this.salary = validatesalary(salary);
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString() => $"{this.id}, {this.name}, {this.salary:c}"; 
+        public string? validatename(string? name)
+        {
+            return name is null ? null : name.Length <= 10 ? name : name.Substring(0, 10);
+        }
+        public decimal validatesalary(decimal salary)
+        {
+            return salary >= 5000 ? salary : 5000;
+        }
+        #endregion
+
+        //inside the class/struct we deal with attribute but outside it we deal with properties
+    }
+}
--- Session 2 OOP Demo/Program.cs
+++ Session 2 OOP Demo/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Session_2_OOP_Demo
+﻿using Session_2_OOP_Demo.Encapsulation;
+
+namespace Session_2_OOP_Demo
 {
     internal class Program
     {
@@ -7,8 +9,14 @@
             #region Struct
             Point p1 =new Point(3,4); // new -> just for constructor selection
             //declare for object of type Point
-            Console.WriteLine($"{p1.x} {p1.y}"); //3 4
+            //Console.WriteLine($"{p1.x} {p1.y}"); //3 4
             Console.WriteLine(p1.ToString()); //before override method -> namespcae.dtname, after -> x, y
+            #endregion
+
+            #region Encapsulation
+            Employee employee = new Employee(1,"ggg",1000);
+            employee.setid(20000);
+            Console.WriteLine(employee.getid());
             #endregion
         }
     }
