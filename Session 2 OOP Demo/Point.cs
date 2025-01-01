349a59b50d5c4f0dfb52065d0a21cfbc44e18241
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-01 21:04:01 +0200

    Struct

---
+++ Session 2 OOP Demo/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_2_OOP_Demo
+{
+    internal struct Point
+    {
+        public int x, y;
+
+        //constructor: is a special function 
+        //            1. named always after the struct or class name
+        //            2. has no return type]
+        public Point()
+        {
+            x = 1;
+            y = 1;
+        }
+        public Point(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return $"{x}, {y}";
+        }
+    }
+}
---
+++ Session 2 OOP Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Session_2_OOP_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Struct
+            Point p1 =new Point(3,4); // new -> just for constructor selection
+            //declare for object of type Point
+            Console.WriteLine($"{p1.x} {p1.y}"); //3 4
+            Console.WriteLine(p1.ToString()); //before override method -> namespcae.dtname, after -> x, y
+            #endregion
+        }
+    }
+}
